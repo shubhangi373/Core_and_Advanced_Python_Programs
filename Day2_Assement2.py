19a769476eb8f8b2a192de38b32fbb5d2aec8a24
shubhangi373 <pawarshubhangi211@gmail.com>
2025-02-11 22:13:47 +0530

    Add files via upload

---
+++ Day2_Assement2.py
@@ -0,0 +1,9 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Declare two variables and print that which variable is largest using ternary operators
+>>> a=5
+>>> b=8
+>>> largest = a if a>b else b
+>>> print("The largest value is:",largest)
+The largest value is: 8
+>>> 
