03c993e61da450cf6966aadce3b1d5f102fe2eef
shubhangi373 <pawarshubhangi211@gmail.com>
2025-02-11 22:15:00 +0530

    Add files via upload

---
+++ Day2_Assement1.py
@@ -0,0 +1,12 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #calculate the multiplication and sum of two numbers
+>>> a=5
+>>> b=7
+>>> c=a*b
+>>> d=a+b
+>>> print("multiplication of a&b is :",c)
+multiplication of a&b is : 35
+>>> print("Sum of a&b is:",d)
+Sum of a&b is: 12
+>>> 
