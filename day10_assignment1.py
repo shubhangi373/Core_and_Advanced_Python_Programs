9ba8752e1a484a895760a7cb2ec8c3f71648f4be
shubhangi373 <pawarshubhangi211@gmail.com>
2025-02-18 11:41:53 +0530

    Add files via upload

---
+++ day10_assignment1.py
@@ -0,0 +1,12 @@
+#python program to sum all the items in a list
+
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Calculate the sum of all items in the list
+total_sum = sum(numbers)
+
+# Print the result
+print("The sum of all items in the list is:", total_sum)
+
+#output: The sum of all items in the list is: 15
